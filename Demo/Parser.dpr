c542694d8e7b3d95f92367335da549d3e8416fbe
Nuno Picado <geral@nunopicado.com>
2017-12-13 23:50:46 +0000

    Cosmetic

--- Demo/Parser.dpr
+++ Demo/Parser.dpr
@@ -7,46 +7,47 @@ program ParserTest;
 {$endif}
 
 uses
-  SysUtils,
-  crt32,
-  ExpressionParser in '..\src\ExpressionParser.pas';
+    SysUtils
+  , Crt32
+  , ExpressionParser in '..\src\ExpressionParser.pas'
+  ;
 
 var
-   Calculadora:ShortString;
-   Parser: TParser;
+  Calculadora : ShortString;
+  Parser      : TParser;
 
 begin
-     if paramcount>0
+  if ParamCount > 0
+    then begin
+      if (ParamStr(1) = '/?') or (ParamStr(1) = '-?')
         then begin
-                  if (ParamStr(1)='/?') or (ParamStr(1)='-?')
-                     then begin
-                               clrscr;
-                               Writeln(Parser.Help(0));
-                          end
-                     else Writeln('Resultado: ',Parser.ParseF(ParamStr(1),3,2));
-                  repeat until keypressed;
-                  Halt(0);
-             end;
+          ClrScr;
+          WriteLn(Parser.Help(0));
+        end
+        else Writeln('Resultado: ', Parser.ParseF(ParamStr(1), 3, 2));
+      repeat until KeyPressed;
+      Halt(0);
+    end;
 
-     try
-        repeat
-              clrscr;
-              Write('Expressao: ');
-              readln(Calculadora);
+  try
+    repeat
+      ClrScr;
+      Write('Expressao: ');
+      ReadLn(Calculadora);
 
-              Parser.Clear;
+      Parser.Clear;
 
-              GotoXY(1,5);
-              Writeln('Resultado: ',Parser.ParseF(Calculadora,3,2));
+      GotoXY(1, 5);
+      WriteLn('Resultado: ', Parser.ParseF(Calculadora, 3, 2));
 
-              GotoXY(1,7);
-              Writeln(Parser.HistoryShow);
+      GotoXY(1, 7);
+      Writeln(Parser.HistoryShow);
 
-              GotoXY(1,25);
-              Write('Continuar [s/N]? ');
-        until UpCase(ReadKey) IN [#27,'N'];
-     except
-        on E: Exception do
-           Writeln(E.ClassName, ': ', E.Message);
-     end;
+      GotoXY(1, 25);
+      Write('Continuar [s/N]? ');
+    until UpCase(ReadKey) in [#27, 'N'];
+  except
+    on E: Exception do
+      WriteLn(E.ClassName, ': ', E.Message);
+  end;
 end.
